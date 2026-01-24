dba2df8f2b5a07a00ece292ef886a35c3eb47a08
raf945 <rafael@prchristensen.com>
2026-01-24 14:20:38 +0000

    Reproducible now

--- DataSetGenerator.py
+++ DataSetGenerator.py
@@ -3,7 +3,9 @@ import argparse
 import os
 from math import floor, ceil
 import random
+#import numpy as np
 
+random.seed(1)
 
 class DataSetGenerator():
 
@@ -25,28 +27,30 @@ class DataSetGenerator():
         self.timeRandom = random.randrange(5, 50, 5)
 
         # Priority
+        """
         z = random.random() # Return float between 0.0 and 1.0
         if z > 0.5:
             self.priority = ceil(random.uniform(1, 5))
         else:
             self.priority = floor(random.uniform(1, 5))
+        """
+        possible_priority = [1, 2, 3, 4, 5]
+        self.priority = random.choices(possible_priority, weights=(10, 8, 6, 4, 2), k=1)
 
         # Tools
         possible_tools = [1, 2, 3]
-        self.toolRandom = random.sample(possible_tools, 1)
+        self.toolRandom = random.choices(possible_tools, k=1)
 
         print(self.machineRandom)
         print(f'time: {self.timeRandom}')
         print(f'priority: {self.priority}')
         print(f'tools: {self.toolRandom}')
-        
-        # return machineRandom, timeRandom, priority, toolRandom
 
     def generateDicts(self):
         self.jobCounter+=1
         
         jobDict = {"job": 'J'+ str(self.jobCounter), "machine": 'M'+ str(self.machineRandom[0]), 
-                   "time": self.timeRandom, "priority": self.priority,
+                   "time": self.timeRandom, "priority": self.priority[0],
                    "tool": 'T'+ str(self.toolRandom[0])
                    }      
         return jobDict
@@ -67,5 +71,4 @@ example.runDataSet()
 
 for x in example.jobPermutations:
     print(x)
-
     
